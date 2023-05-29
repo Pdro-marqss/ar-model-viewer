d7337bd730f69a48bb4348ea9e7a1334e24294d0
Pedro <pedro.marques@bizsys.com.br>
2023-05-29 16:45:17 -0300

    template com AR televisao

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: { https:true },
+  plugins: [mkcert()],
+})
